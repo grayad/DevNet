0b370f28fdfa59d7c4cee4d4eef29311856f7e0c
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-09-15 20:56:54 -0400

    add query to get single user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,6 +17,9 @@ const resolvers = {
     users: async () => {
       return User.find().select("-__v -password");
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).select("-__v -password");
+    },
   },
   Mutation: {
     addUser: async (parent, args) => {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,6 +24,7 @@ const typeDefs = gql`
   type Query {
     me: User
     users: [User]
+    user(username: String!): User
   }
 
   type Mutation {
