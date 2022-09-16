246e2ce3ccc0d5b4a6921a89bec85dfeaed877bd
grayad <102432930+grayad@users.noreply.github.com>
2022-09-15 21:12:45 -0400

    Revert "Started login component"

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-
-import Login from "./components/Login";
-
 function App() {
   return (
-    <div>
-      <Login />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
