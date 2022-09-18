0f1066bbed73f29f628f8d8b175cfd89ffd71050
Your Name <you@example.com>
2022-09-17 23:25:08 -0400

    Separate login and signup

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+
 const Login = () => {
   return (
 		<main className='login'>
             <h1 class="text-center">Welcome to DevNet</h1>
             <div className="col-12 col-md-6 mx-auto">
                 <div className="card">
-                    <h4 className="card-header">Login/Sign-Up</h4>
+                    <h4 className="card-header">Sign-Up</h4>
                     <div className="card-body">
                         <form>
                         <input
@@ -25,34 +26,8 @@ const Login = () => {
                             id="password"
                         />
                         </form>
-                        <h2 class="text-center"> Are you a Developer or a Company?</h2>
-                        <div class="form-check">
-                            <input
-                                class="form-check-input"
-                                type="radio"
-                                name="radio"
-                                id="devRadio"
-                                value="option1"
-                                checked>
-                            </input>
-                            <label class="form-check-label" for="devRadio">
-                            Developer
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input
-                                class="form-check-input"
-                                type="radio"
-                                name="radio"
-                                id="compRadio"
-                                value="option2">
-                            </input>
-                            <label class="form-check-label" for="compRadio">
-                            Company
-                            </label>
-                        </div>
                         <div class="text-center">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary btn-dark">Submit</button>
                         </div>
                     </div>
                 </div>
