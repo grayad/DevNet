fc393b38c337d4405979825e53819dd4047c0634
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-09-18 16:03:00 -0400

    add apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
 import Login from "./components/Login";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Login />
+      </div>
+    </ApolloProvider>
   );
 }
 
