12b96cc4112134f7826cb728824fd00be6bb6d3b
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-09-22 11:10:19 -0400

    render jobs

--- client/src/components/pages/Jobs.js
+++ client/src/components/pages/Jobs.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { QUERY_USERS } from "../../utils/queries";
-// import { QUERY_JOBS } from "../../utils/queries";
+import { QUERY_JOBS } from "../../utils/queries";
 import Auth from "../../utils/auth";
 
 const Jobs = () => {
   // use useQuery hook to make query request for all users
-  const { loading, data } = useQuery(QUERY_USERS);
+  const { loading, data } = useQuery(QUERY_JOBS);
 
-  const users = data?.users;
+  const jobs = data?.jobs;
 
   //   if user is not logged in, redirect page to login
   if (!Auth.loggedIn()) {
     return <Navigate to="/login" />;
   }
-  console.log(users);
+  console.log(jobs);
 
   return (
     <div>
@@ -29,31 +28,29 @@ const Jobs = () => {
               <div>Loading...</div>
             ) : (
               <div className="jobs-list">
-                {users
-                  .filter((user) => user.type === "Company")
-                  .map((Company) => (
+                {jobs
+                  .map((job) => (
                     <div className="card mb-3">
-                      <p className="card-header">{Company.username} </p>
+                      <p className="card-header">{job.jobTitle} </p>
                       <div className="card-body">
                         <p>
                           Company Name:{" "}
-                          {Company.title ? Company.title : "No company name yet"}
+                          {job.companyName ? job.companyName : "No company name yet"}
                         </p>
                         <p>
                           Job Description:{" "}
-                          {Company.bio ? Company.bio : "No job description yet"}
+                          {job.jobDescription ? job.jobDescription : "No job description yet"}
                         </p>
                         <div className="d-flex align-items-center">
                           <p>Required Skills:</p>{" "}
-                          {Company.skills
-                            ? Company.skills.map((skill) => (
+                          {job.skills
+                            ? job.skills.map((skill) => (
                                 <p className="p-1">{skill}</p>
                               ))
                             : "No required skills yet"}
                         </div>
                         <div className="d-flex justify-content-end">
-                          <button className="m-2 p-1">See Profile</button>
-                          <button className="m-2 p-1">Connect</button>
+                          <button className="m-2 p-1">Delete Job</button>
                         </div>
                       </div>
                     </div>
