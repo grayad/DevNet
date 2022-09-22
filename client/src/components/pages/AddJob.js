c9ed467a193e9a7e287065dc0c33545a4bbb9e30
hlnicks <hlnicks@icloud.com>
2022-09-22 07:20:39 -0400

    worked on handles and added code to use as template

--- client/src/components/pages/AddJob.js
+++ client/src/components/pages/AddJob.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { ADD_JOB } from '../../utils/mutations';
 import { useQuery, useMutation } from '@apollo/client';
-
-
+import { QUERY_JOBS } from '../../utils/queries';
 
 
 const Jobform = () => {
   const [addJob] = useMutation(ADD_JOB);
 
 
-
   const handleClick = async () => {
     try {
       await addJob({
-        variables: { id: user._id },
+        variables: { addJob: _id },
       });
     } catch (e) {
       console.error(e);
@@ -22,6 +20,11 @@ const Jobform = () => {
 
   // update state based on form input changes
   const handleChange = (event) => {
+    const { name, value } = event.target;
+    setuserFormData({
+      // ...userFormData,
+      [name]: value,
+    });
   };
 
   // submit form
@@ -84,3 +87,29 @@ const Jobform = () => {
 };
 
 export default Jobform;
+
+
+// {user.username && (
+//   <>
+//     <div className="col-12 col-md-9 mx-auto py-4">
+//         <div className="card">
+          // <h4 className="card-header">{$jobTitle}</h4>
+//           <div className="card-body">
+//             <h4>Company Name: {user?.username}</h4>
+//             <h4>Job Description: {user?.type}</h4>
+//             <h4>Title: {user?.title}</h4>
+//             <h4>Bio: {user?.bio}</h4>
+//             <h4>Skills: {user?.skills}</h4>
+//           </div>
+//         </div>
+//       <div className="col-12 col-lg-12 mb-3">
+//         <ConnectionList
+//           username={user?.username}
+//           connectionCount={user?.connectionCount}
+//           connections={user?.connections}
+//         />
+//       </div>
+//     </div>
+//     <div className="mb-3">{!userParam}</div>
+//   </>
+// )}
