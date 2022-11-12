97a5ac21c63cbf8824aefb4a9aa627d60b10407e
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-11-12 17:03:33 -0500

    fix queries in resolvers by adding .populate(connections)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,19 +6,21 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("connections");
 
         return userData;
       }
       throw new AuthenticationError("Not logged in!");
     },
     users: async () => {
-      return User.find().select("-__v -password");
+      return User.find().select("-__v -password").populate("connections");
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).select("-__v -password");
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("connections");
     },
     jobs: async () => {
       return Job.find().select("-__v");
